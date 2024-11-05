2908a54b9607ed0021a6ded8f1bddc42dbc8cb2e
ephe-meral <j.appel.online@gmail.com>
2024-11-05 14:46:20 -0700

    fix cache issue

--- app/page.js
+++ app/page.js
@@ -2,5 +2,12 @@ import { getDefaultLanguage } from "@/i18n"
 
 export default function Page() {
   const lang = getDefaultLanguage()
-  return (<head><meta httpEquiv='Refresh' content={`0; url='/${lang}'`} /></head>)
+  return (
+    <head>
+      <meta httpEquiv='cache-control' content='no-cache' />
+      <meta httpEquiv='expires' content='0' />
+      <meta httpEquiv='pragma' content='no-cache' />
+      <meta httpEquiv='Refresh' content={`0; url='/${lang}'`} />
+    </head>
+  )
 };
